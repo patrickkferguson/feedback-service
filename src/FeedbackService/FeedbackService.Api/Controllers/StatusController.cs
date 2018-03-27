8d0e2fff905b6b3bd330954acc6cca6e33c590d7
patrickkferguson <patrickkferguson@gmail.com>
2018-03-27 23:03:40 +1100

    Add StatusController for AWS ELB healthcheck.

---
+++ src/FeedbackService/FeedbackService.Api/Controllers/StatusController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FeedbackService.Api.Controllers
+{
+    [Route("status")]
+    public class StatusController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Ok");
+        }
+    }
+}
